60c736d41709e1a22e1b150fc72b54840d257cf8
Dale Ghent <daleg@elemental.org>
2021-11-19 23:48:20 -0500

    Update plugin description

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -56,16 +56,18 @@ Please refer to this [plugin's website](https://daleghent.com/utilities-for-astr
 
 # Requirements #
 
-* NINA 1.11
-* APCC Pro 1.9 or APCC Standard 1.8 (APCC Pro is required for the use of APPM)
+* NINA 2.0
+* APCC Pro 1.9.2.3 or later
 * The **Enable Server** setting is **On** in NINA's options (refer to Options > General)
 
 # Provided functions #
 
 * Start APCC
     * Starts APCC and connects NINA to the Astro-Physics ASCOM driver. Assumes that APCC's **Auto-Connect** setting is selected in both the **Mount** and **AP V2 Driver** option areas in the Setup tab
-* Create APPM Model
-    * A sequence instruction that will run Astro-Physics Point Mapper (APPM) in an automated mode. When ran, APPM will use its existing default settings to run a point mapping session and will load the results into APCC Pro when complete. If the default settings and point map are not desired, an APPM settings or a point map file may be optionally specified below in this plugin's settings.
+* Create All-Sky Model
+    * A sequence instruction that will run Astro-Physics Point Mapper (APPM) in an automated mode. When ran, APPM will use its existing default settings to run an all-sky point mapping session and will load the results into APCC Pro when complete. If the default settings and point map are not desired, an APPM settings or a point map file may be optionally specified below in this plugin's settings.
+* Create Dec Arc Model
+    * Automatically generates a dec arc model based on several parameters for the target that the instruction is subordinate to
 
 # Getting help #
 
