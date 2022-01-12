45f07e7babdfef5565b7e52c862ff38ec5fbb60d
Dale Ghent <daleg@elemental.org>
2022-01-12 02:14:56 -0500

    Update to NINA 2.0 21

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2021 Dale Ghent")]
 
 // The minimum Version of N.I.N.A. that this plugin is compatible with
-[assembly: AssemblyMetadata("MinimumApplicationVersion", "2.0.0.2020")]
+[assembly: AssemblyMetadata("MinimumApplicationVersion", "2.0.0.2021")]
 
 // The license your plugin code is using
 [assembly: AssemblyMetadata("License", "MPL-2.0")]
